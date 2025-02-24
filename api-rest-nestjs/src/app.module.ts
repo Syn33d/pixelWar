e21cdfa7a9f714f4a43f92a26afae608b593bbb1
Syn33d <128500319+Syn33d@users.noreply.github.com>
2025-02-24 11:40:44 +0100

    Add typeorm on app.module

--- api-rest-nestjs/src/app.module.ts
+++ api-rest-nestjs/src/app.module.ts
@@ -4,9 +4,18 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { CanvasModule } from './canvas/canvas.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UserModule, CanvasModule, AuthModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || 3306),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,}),
+    UserModule, CanvasModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
