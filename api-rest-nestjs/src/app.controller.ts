4d48f3b6bc9efd6c5607eff7387f9b4ff1e29350
Mathieu <mathieuxavier38150@gmail.com>
2025-02-26 16:03:48 +0100

    wip

--- api-rest-nestjs/src/app.controller.ts
+++ api-rest-nestjs/src/app.controller.ts
@@ -1,12 +1,42 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Res } from '@nestjs/common';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Res() res: Response): void {
+    res.send(`
+                                          <!DOCTYPE html>
+                                            <html lang="en">
+                                            <head>
+                                              <title>Canvas Drawing</title>
+                                              <script src="/socket.io/socket.io.js"></script>
+                                            </head>
+                                            <body>
+                                              <input type="color" id="colorPicker" value="#000000">
+                                              <canvas id="myCanvas" width="400" height="400" style="border:1px solid #000000;"></canvas>
+                                              <script>
+                                                const canvas = document.getElementById('myCanvas');
+                                                const context = canvas.getContext('2d');
+                                                const socket = io();
+                                                const pixelSize = 10;     
+                                                const colorPicker = document.getElementById('colorPicker');
+                                      
+                                                canvas.addEventListener('click', (event) => {
+                                                  const rect = canvas.getBoundingClientRect();
+                                                  const x = Math.floor((event.clientX - rect.left) / pixelSize) * pixelSize;
+                                                  const y = Math.floor((event.clientY - rect.top) / pixelSize) * pixelSize;
+                                                  const color = colorPicker.value;
+                                                  socket.emit('updatePixel', { x, y, color });
+                                                });
+                                      
+                                                socket.on('pixelUpdated', (data) => {
+                                                  context.fillStyle = data.color;
+                                                  context.fillRect(data.x, data.y, pixelSize, pixelSize);
+                                                });
+                                              </script>
+                                            </body>
+                                            </html>
+                                          `);
   }
 }
