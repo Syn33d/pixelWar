d5ebd13627715075651f1c478f28c0efba62cb47
Clément SEUX <clement.seux@axxes.fr>
2025-04-04 13:37:48 +0200

    Log database connection details during application startup

--- api-rest-nestjs/src/main.ts
+++ api-rest-nestjs/src/main.ts
@@ -6,6 +6,11 @@ import * as path from 'path';
 
 // Explicitly specify the path to the .env file in the parent directory
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_PORT:', process.env.DB_PORT);
+console.log('DB_USERNAME:', process.env.DB_USERNAME);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_DATABASE:', process.env.DB_DATABASE);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
